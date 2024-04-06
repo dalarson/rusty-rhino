5079f3e89a9a10762586dc459aeb2000f366fb93
David Larson <david.larson@nuance.com>
2024-04-06 16:35:51 -0400

    implement auth component shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { MantineProvider } from '@mantine/core'
+import { AuthProvider } from './auth/AuthProvider'
+import "@mantine/core/styles.css"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme='dark'>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </MantineProvider>
   </React.StrictMode>,
 )
