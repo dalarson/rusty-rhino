93c370c1fd8ad7aea07e23f9f4b44dceb7528c12
David Larson <larson.david46@gmail.com>
2025-03-03 19:08:30 -0800

    fix build

--- src/auth/DefaultUserContext.ts
+++ src/auth/DefaultUserContext.ts
@@ -4,6 +4,7 @@ export const defaultUserContext: IUserContext = {
     isAdmin: false,
     name: undefined,
     login: () => {
+        return Promise.resolve();
         // do nothing
     },
     logout: () => {
