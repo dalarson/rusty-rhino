8d5a6fffefba5491344328c5ba1e9c51798eabd9
David Larson <larson.david46@gmail.com>
2025-03-03 19:35:12 -0800

    restore login folder

---
+++ src/header/login/ILoginDialogProps.ts
@@ -0,0 +1,19 @@
+import { ICredentials } from "./ICredentials";
+
+export interface ILoginDialogProps {
+    /**
+     * Whether the dialog is open or not.
+     */
+    isOpen: boolean;
+    /**
+     * A callback to close the dialog.
+     * @returns - Nothing.
+     */
+    onClose: () => void;
+    /**
+     * A callback function for when a user submits the login dialog.
+     * @param credentials - The credentials the user wishes to login with.
+     * @returns - Nothing.
+     */
+    onSubmit: (credentials: ICredentials) => Promise<void>;
+}
---
+++ src/header/login/UserButton.tsx
@@ -0,0 +1,43 @@
+import { UnstyledButton, Group, Menu } from "@mantine/core";
+import { IconChevronDown } from "@tabler/icons-react";
+import { IUserButtonProps } from "./IUserButtonProps";
+import { Text } from "@mantine/core"
+import { useAuth } from "../../auth/useAuth";
+
+
+export const UserButton = (props: IUserButtonProps): JSX.Element => {
+    const authContext = useAuth();
+
+    if (authContext.isAdmin) {
+        return (
+            <Menu
+                width={260}
+                position="bottom-end"
+                transitionProps={{ transition: 'pop-top-right' }}
+                withinPortal
+            >
+                <Menu.Target>
+                    <UnstyledButton>
+                        <Group gap={7}>
+                            <Text fw={500} size="sm" lh={1} mr={3}>
+                                {authContext.name}
+                            </Text>
+                            <IconChevronDown size={12} stroke={1.5} />
+                        </Group>
+                    </UnstyledButton>
+                </Menu.Target>
+                <Menu.Dropdown>
+                    <Menu.Label onClick={props.onSignOutClick}>Sign out</Menu.Label>
+                </Menu.Dropdown>
+            </Menu>
+        )
+    } else {
+        return (
+            <UnstyledButton onClick={() => {
+                props.onSignInClick();
+            }}>
+                <Text fw={500} size="sm" lh={1} mr={3}>Sign in</Text>
+            </UnstyledButton>
+        )
+    }
+}
