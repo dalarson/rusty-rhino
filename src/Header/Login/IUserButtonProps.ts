4d7bf7e1ce63c8299722a984bb2a80f23362b0fa
David Larson <larson.david46@gmail.com>
2025-03-03 19:26:35 -0800

    fix casing issues

--- src/Header/Login/IUserButtonProps.ts
+++
@@ -1,6 +0,0 @@
-export interface IUserButtonProps {
-    // callback to sign in a new user
-    onSignInClick: () => void;
-    // callback to sign out the currently signed in user
-    onSignOutClick: () => void;
-}
